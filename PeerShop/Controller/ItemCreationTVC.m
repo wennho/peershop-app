81e488bbbad6d3161e6aed66993ebcd538670801
Wen Hao <wennho@gmail.com>
2014-05-31 19:19:08 -0700

    update

--- PeerShop/Controller/ItemCreationTVC.m
+++ PeerShop/Controller/ItemCreationTVC.m
@@ -8,15 +8,20 @@
 
 #import "ItemCreationTVC.h"
 #import "PeerShopInterface.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ItemCreationTVC ()
+@interface ItemCreationTVC () <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *itemTitle;
 @property (weak, nonatomic) IBOutlet UITextField *itemPrice;
 @property (weak, nonatomic) IBOutlet UITextView *itemDescription;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ItemCreationTVC
+
+#define IMAGE_ROW 4
 
 - (void) viewDidLoad {
     [super viewDidLoad];
@@ -32,6 +37,50 @@
 //      };
 }
 
+- (IBAction)getGalleryImage:(id)sender {
+    [self getImage:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+- (IBAction)takePhoto:(id)sender {
+    [self getImage:UIImagePickerControllerSourceTypeCamera];
+}
+
+-(void) getImage:(UIImagePickerControllerSourceType) sourceType
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.mediaTypes = @[(NSString *)kUTTypeImage];
+    picker.sourceType = sourceType;
+    picker.allowsEditing = NO;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.imageView.image = info[UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+}
 
 
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == IMAGE_ROW){
+        if (self.imageView.image){
+            CGSize size = self.imageView.image.size;
+            CGFloat aspectRatio = size.height / size.width;
+            return aspectRatio * self.imageView.frame.size.width;
+        } else {
+            return 50;
+        }
+    } else {
+        return self.tableView.rowHeight;
+    }
+}
 @end
